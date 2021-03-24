9bd39db98bbfec5fdeef36e84033281d5a0872dd
Reese Avery Kling <raveryk@gmail.com>
2021-03-23 19:40:37 -0500

    Q1-5 Tues PM

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -51,7 +51,7 @@ console.log(me.favThreeFoods[me.favThreeFoods.length-1]);
 
 console.log(me.shoeCount);
 
-
+me.shoeCount = 5 + 1;
 
 console.log(me.shoeCount);
 
@@ -62,3 +62,7 @@ console.log(me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'green';
+
+console.log(me.favoriteColor);
